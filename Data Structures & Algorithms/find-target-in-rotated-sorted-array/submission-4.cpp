610622871c6827dad6e12a704f6b5cef87249bf1
grnac43 <59119772+grnac43@users.noreply.github.com>
2026-08-17 22:53:00 +0200

    Add: find-target-in-rotated-sorted-array - submission-4

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-4.cpp
@@ -0,0 +1,45 @@
+class Solution {
+
+    int findPivot(vector<int>& nums){
+        int l = 0;
+        int r = nums.size() - 1;
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+
+            if (nums[mid] > nums[r])
+                l = mid + 1;
+            else
+                r = mid;
+        }
+
+        return l;
+    }
+
+    int binarySearch(vector<int>& nums, int left, int right, int target){
+
+        while(left<=right){
+            int mid = (left+right)/2;
+
+            if(nums[mid] > target)
+                right = mid-1;
+            else if(nums[mid] < target)
+                left = mid+1;
+            else return mid;
+        }
+        return -1;
+    }
+public:
+    int search(vector<int>& nums, int target) {
+        int pivot = findPivot(nums);
+
+        int ret = binarySearch(nums, 0, pivot-1, target);
+    
+        if(ret!=-1)
+            return ret;
+        
+        ret = binarySearch(nums, pivot, nums.size()-1, target);
+
+        return ret;
+    }
+};
